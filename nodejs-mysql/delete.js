4848a81ded383027227ec70d88519c1b5b9d74fe
zelitosaide <zelitosaide83@gmail.com>
2022-12-06 12:49:43 +0200

    delete

--- nodejs-mysql/delete.js
+++ nodejs-mysql/delete.js
@@ -1,2 +1,10 @@
 import mysql from "mysql";
 import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});

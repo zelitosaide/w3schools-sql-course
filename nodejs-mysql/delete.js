bdf223d81ab5cb9cea499452099b5f93909a277a
zelitosaide <zelitosaide83@gmail.com>
2022-12-06 12:54:41 +0200

    delete

--- nodejs-mysql/delete.js
+++ nodejs-mysql/delete.js
@@ -8,3 +8,14 @@ const connection = mysql.createConnection({
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
 });
+
+export function deleteRow() {
+  connection.connect(function (error) {
+    if (error) throw error;
+    const sql = `DELETE FROM customers WHERE address = "Mountain 21"`;
+    connection.query(sql, function (error, result) {
+      if (error) throw error;
+      console.log("Number of records deleted: " + result.affectedRows);
+    });
+  });
+}

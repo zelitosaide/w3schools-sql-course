e34b03ad774609d25d41d01c6cb7e5cf7a5067b4
zelitosaide <zelitosaide83@gmail.com>
2022-12-06 04:07:58 +0200

    insert into

---
+++ lessons/insert-into.js
@@ -0,0 +1,21 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+export function insertInto() {
+  connection.connect(function (error) {
+    if (error) throw error;
+    const sql = `INSERT INTO customers (name, adddress) VALUES ("Company Inc", "Highway 37")`;
+    connection.query(sql, function (error, result) {
+      if (error) throw error;
+      console.log(result);
+    });
+  });
+}

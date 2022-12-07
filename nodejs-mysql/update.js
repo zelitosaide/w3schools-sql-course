bc3bd682285bb62c07259cd6570fb2697517f8a9
zelitosaide <zelitosaide83@gmail.com>
2022-12-07 09:56:53 +0200

    update

--- nodejs-mysql/update.js
+++ nodejs-mysql/update.js
@@ -2,3 +2,20 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const connection = mysql.createConnection({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+export function update() {
+  connection.connect(function (error) {
+    if (error) throw error;
+    const sql = `UPDATE customers SET address = "Canyon 123" WHERE address = "Valley 345"`;
+    connection.query(sql, function (error, result) {
+      if (error) throw error;
+      console.log(result.affectedRows + " record(s) updated");
+    });
+  });
+}

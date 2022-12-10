5f418dfd632f754655f741ceed5c66984f171e2b
zelitosaide <zelitosaide83@gmail.com>
2022-12-10 09:14:46 +0200

    syntax

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { home } from "./sql-lessons/home/home.js";
 import { intro } from "./sql-lessons/intro/intro.js";
+import { syntax } from "./sql-lessons/syntax/syntax.js";
 
-intro();
+syntax();

6a45bd55c9e1fc4e9e43d8c3ddbcb91d34048785
zelitosaide <zelitosaide83@gmail.com>
2022-12-26 14:31:28 +0200

    SELECT DISTINCT

--- sql-lessons/select-distinct/select-distinct.js
+++ sql-lessons/select-distinct/select-distinct.js
@@ -1,6 +1,7 @@
 import { connect } from "../../utils/db.config.js";
 
 export async function selectDistinct() {
+  // SELECT DISTINCT column1, column2, ... FROM table_name;
   try {
     const { connection } = await connect();
     const sql = "SELECT DISTINCT address FROM customers;";

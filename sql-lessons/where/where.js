cbdda580366415c848ed0dbc6723a080acf6133e
zelitosaide <zelitosaide83@gmail.com>
2022-12-27 14:14:40 +0200

    WHERE

--- sql-lessons/where/where.js
+++ sql-lessons/where/where.js
@@ -1,10 +1,14 @@
 import { connect } from "../../utils/db.config.js";
 
 export async function where() {
-  const { connection } = await connect();
-  const sql = 'SELECT * FROM customers WHERE address="Namicopo"';
-  connection.query(sql, function (error, result) {
-    if (error) throw error;
-    console.log(result);
-  });
+  try {
+    const { connection } = await connect();
+    const sql = 'SELECT * FROM customers WHERE address="Namicopo"';
+    connection.query(sql, function (error, result) {
+      if (error) throw error;
+      console.log(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }

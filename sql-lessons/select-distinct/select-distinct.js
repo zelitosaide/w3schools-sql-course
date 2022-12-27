75909d6dbcda985ef4971283dcdbf804318ee565
zelitosaide <zelitosaide83@gmail.com>
2022-12-27 05:22:23 +0200

    SELECT DISTINCT

--- sql-lessons/select-distinct/select-distinct.js
+++ sql-lessons/select-distinct/select-distinct.js
@@ -54,10 +54,14 @@ export async function selectAllCountAndRenameColumn() {
 }
 
 export async function selectDistinctAndCount() {
-  const { connection } = await connect();
-  const sql = "SELECT COUNT(DISTINCT address) FROM customers;";
-  connection.query(sql, function (error, result) {
-    if (error) throw error;
-    console.log(result);
-  });
+  try {
+    const { connection } = await connect();
+    const sql = "SELECT COUNT(DISTINCT address) FROM customers;";
+    connection.query(sql, function (error, result) {
+      if (error) throw error;
+      console.log(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }

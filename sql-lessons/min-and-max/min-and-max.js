70e8ae83acfd75b87b92828cdcfdabfd384ff042
zelitosaide <zelitosaide83@gmail.com>
2023-01-04 09:47:32 +0200

    MIN AND MAX

--- sql-lessons/min-and-max/min-and-max.js
+++ sql-lessons/min-and-max/min-and-max.js
@@ -4,3 +4,8 @@ export async function minId() {
   const [result] = await query("SELECT MIN(id) AS minId FROM customers");
   return result.minId;
 }
+
+export async function maxId() {
+  const [result] = await query("SELECT MAX(id) AS maxId FROM customers");
+  return result.maxId;
+}

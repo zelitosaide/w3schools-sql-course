840d69db8395891727f9ee82006fff3a065d0ae3
zelitosaide <zelitosaide83@gmail.com>
2023-01-04 09:44:04 +0200

    MIN AND MAX

--- sql-lessons/min-and-max/min-and-max.js
+++ sql-lessons/min-and-max/min-and-max.js
@@ -1,5 +1,6 @@
 import { query } from "../../utils/db.config.js";
 
 export async function minId() {
-  return await query("SELECT MIN(id) AS minId FROM customers");
+  const [result] = await query("SELECT MIN(id) AS minId FROM customers");
+  return result.minId;
 }
